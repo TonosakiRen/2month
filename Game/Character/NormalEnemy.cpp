0c6ab6c36cde5f70edfe57fdab1f9672876d39b8
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-01-16 12:09:26 +0900

    normalEnemyの作成

--- Game/Character/NormalEnemy.cpp
+++ Game/Character/NormalEnemy.cpp
@@ -1,31 +1,45 @@
 #include "NormalEnemy.h"
 #include "ModelManager.h"
 
-void NormalEnemy::Initialize(Vector3 scale, Quaternion quaternion, Vector3 translate) {
-	GameObject::Initialize("enemy");
+void NormalEnemy::Initialize(const Vector3& scale, const Quaternion& quaternion, const Vector3& translate) {
+	GameObject::Initialize("player");
 	worldTransform_.scale_ = scale;
 	worldTransform_.quaternion_ = quaternion;
 	worldTransform_.translation_ = translate;
-	UpdateMatrix();
+	worldTransform_.Update();
 	rotate = EulerAngle(worldTransform_.quaternion_);
 	rotate.x = Degree(rotate.x) - 180.0f; rotate.y = Degree(rotate.y) - 180.0f; rotate.z = Degree(rotate.z) - 180.0f;
 
 	Vector3 modelSize = ModelManager::GetInstance()->GetModelSize(modelHandle_);
 	collider_.Initialize(&worldTransform_, "Enemy", modelHandle_);
+
+	// とりあえず一個だけ
+	auto& modelTrans = modelTransform_.emplace_back(WorldTransform());
+	modelTrans.Initialize();
+	modelTrans.SetParent(&worldTransform_);
+	modelTrans.Update();
+
 }
 
-void NormalEnemy::Update() {
+void NormalEnemy::Update(const float& sensedDistance) {
 	DrawImGui();
 
 	Move();
 
-	collider_.AdjustmentScale();
-	UpdateMatrix();
+	
+	UpdateTransform();
+}
+
+void NormalEnemy::OnCollision(Collider& collider) {
+	Vector3 pushBackVector;
+	if (collider_.Collision(collider, pushBackVector)) {
+		
+	}
 }
 
 void NormalEnemy::Draw() {
 	collider_.Draw();
-	GameObject::Draw();
+	GameObject::Draw(modelTransform_.at(0), color);
 }
 
 void NormalEnemy::Move() {
@@ -36,9 +50,18 @@ void NormalEnemy::Move() {
 		flag = !flag;
 		count = 0;
 	}
+	const float speed = 0.5f;
 
-	flag ? vel.x = -1.0f : vel.x = 1.0f;
+	flag ? vel.x = -speed : vel.x = speed;
 
 	worldTransform_.translation_ += vel;
 	UpdateMatrix();
+}
+
+void NormalEnemy::UpdateTransform() {
+	collider_.AdjustmentScale();
+	worldTransform_.Update();
+	for (auto& model : modelTransform_) {
+		model.Update();
+	}
 }
--- Game/Character/NormalEnemy.h
+++ Game/Character/NormalEnemy.h
@@ -1,16 +1,21 @@
 #pragma once
 #include "BaseCharacter.h"
+#include <vector>
 
 class NormalEnemy : public BaseCharacter {
 public: // 仮想関数
-	void Initialize(Vector3 scale, Quaternion quaternion, Vector3 translate) override;
-	void Update() override;
+	void Initialize(const Vector3& scale, const Quaternion& quaternion, const Vector3& translate) override;
+	void Update(const float& sensedDistance) override;
+	void OnCollision(Collider& collider) override;
 	void Draw() override;
 
 private:
 	void Move();
+	void UpdateTransform();
 
 	int count = 0;
+	std::vector<WorldTransform> modelTransform_;
+	Vector4 color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
 
 
 };
