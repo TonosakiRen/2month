1a81abdce0b48d28df6bcf9624489af7356a8af8
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-01-22 22:24:03 +0900

    BaseCharacterの追加

---
+++ Game/Character/BaseCharacter.cpp
@@ -0,0 +1,41 @@
+#include "BaseCharacter.h"
+
+void BaseCharacter::DrawImGui() {
+#ifdef _DEBUG
+	ImGui::DragFloat3("scale", &worldTransform_.scale_.x, 0.1f);
+	ImGui::DragFloat3("rotate", &rotate.x, 0.1f, -360.0f, 360.0f);
+	Vector3 handle = Vector3(Radian(rotate.x), Radian(rotate.y), Radian(rotate.z));
+	worldTransform_.quaternion_ = MakeFromEulerAngle(handle);
+	ImGui::DragFloat3("translate", &worldTransform_.translation_.x, 0.1f);
+
+#endif // _DEBUG
+}
+
+void BaseCharacter::CreateInitialize() {
+	buffer_.Create(sizeof(Vector2));
+
+}
+
+void BaseCharacter::BaseInitialize(const uint32_t& modelNumber, std::vector<std::string> names) {
+	models_.resize(modelNumber);
+	modelsTransform_.resize(modelNumber);
+
+	for (uint32_t index = 0u; index < modelNumber; index++) {
+		models_.at(index).Initialize(names.at(index));
+		modelsTransform_.at(index).Initialize();
+	}
+
+}
+
+void BaseCharacter::UpdateTransform() {
+	worldTransform_.Update();
+	for (auto& trans : modelsTransform_) {
+		trans.Update();
+	}
+}
+
+void BaseCharacter::BaseDraw() {
+	for (uint32_t index = 0u; index < models_.size(); index++) {
+		models_.at(index).Draw(modelsTransform_.at(index));
+	}
+}
