bb943cbf6965e59f75775bcfc3c0a029836449fb
KanaiMint <115054962+KanaiMint@users.noreply.github.com>
2024-02-07 04:09:24 +0900

    editerの時音消した

--- GameScene.cpp
+++ GameScene.cpp
@@ -105,9 +105,10 @@ void GameScene::Initialize() {
 
 	// シーンリクエスト
 	// editor使用時のみ初期からDebugCameraを使用
-	sceneRequest_ = Scene::Title;
+	sceneRequest_ = Scene::Editor;
 	if (sceneRequest_ == Scene::Editor) {
 		ViewProjection::isUseDebugCamera = true;
+		audio_->SetValume(bgmPlayHandle, 0.0f);
 	}
 }
 
