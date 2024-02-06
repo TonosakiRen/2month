b4bb55e2cc19b6590190dc2ce1e72a5233adff54
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-02-07 05:21:56 +0900

    editorでplayerの座標を戻さないように変更

--- Game/Scene/CreateStageScene.cpp
+++ Game/Scene/CreateStageScene.cpp
@@ -177,7 +177,7 @@ void CreateStageScene::DrawImGui() {
 					isPlay = false;
 					stage_->Load("temporary");
 					enemy_->Load("temporary");
-					stage_->SetPlayerRespawn(player_.get());
+					//stage_->SetPlayerRespawn(player_.get());
 				}
 			}
 			if (ImGui::TreeNode("FileSave")) {
