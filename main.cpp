3bbb80404e07026e098849ad22b2447bbd7b9dbe
KanaiMint <115054962+KanaiMint@users.noreply.github.com>
2024-02-08 08:51:03 +0900

    タイトルバー変更

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow("2309_シャドォーン");
 
 	renderer = Renderer::GetInstance();
 	renderer->Initialize();
